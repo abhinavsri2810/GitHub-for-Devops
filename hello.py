66f51a168df59d40af191962e130b9bca3b4826d
abhinavsri2810 <abhishri2004@gmail.com>
2025-03-28 00:42:14 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 print("HI,My name is Abhinav Srivastava and I am pursuing a BCA from Shri Ram Swaroop Memorial University")
 
 print("My favourite sports is cricket")
+
+print("No, my name is aditya mishra")
